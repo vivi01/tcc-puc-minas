4fa7c6400650785137cebf8d8f4bd8cff3c7e533
FernandoCic <nandogoncalves86@gmail.com>
2021-12-31 17:10:20 -0300

    Ajustes e inclusão de métodos e suas estruturas.

---
+++ src/ModuloInformacaoCadastral/GISA.Associado/Repositories/AssociadoRepository.cs
@@ -0,0 +1,40 @@
+﻿using GISA.Associado.Entities;
+using GISA.Associado.Enums;
+using GISA.Associado.Repositories.Interfaces;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GISA.Associado.Repositories
+{
+    public class AssociadoRepository : IAssociadoRepository
+    {
+        public async Task<ESituacaoAssociado> GetSituacao(int codigoAssociado)
+        {
+            var situacao = ESituacaoAssociado.Ativo;
+            return JsonConvert.DeserializeObject<ESituacaoAssociado>(situacao.ToString());
+           
+        }
+
+        public async Task<decimal> GetValor()
+        {
+            return 0;
+        }
+
+        //public async Task<string> SolicitarMarcacaoExame(MarcacaoConsulta marcacaoConsulta, string token)
+        //{
+
+        //}
+
+        public async void AlterarPlano(Entities.Associado associado, string token)
+        {
+
+        }
+
+        public async Task<List<Plano>> Getplanosdisponoveis()
+        {
+            List<Plano> planos = new List<Plano>();
+            return planos;
+        }
+    }
+}
