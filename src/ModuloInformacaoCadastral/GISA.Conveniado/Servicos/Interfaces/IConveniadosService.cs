e79bafc1d49c66b622d6679c478ed724a0faffe9
Viviane Pereira <vivianecruz01@hotmail.com>
2022-01-03 17:38:03 -0300

    Removido projeto conveniado

--- src/ModuloInformacaoCadastral/GISA.Conveniado/Servicos/Interfaces/IConveniadosService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GISA.Conveniado.Servicos.Interfaces
-{
-    public interface IConveniadosService
-    {
-        string SolicitarAutorizacoExame(string token, int codigoExame, int codigoAssociado);
-    }
-}
